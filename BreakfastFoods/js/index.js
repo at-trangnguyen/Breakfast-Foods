3f5504a0b30288bca0312c21471e40db6e7ee833
Trang Nguyen <trang.nguyen@asiantech.vn>
2018-07-11 15:36:42 +0700

    remove some unnecessary package and file

--- BreakfastFoods/js/index.js
+++ BreakfastFoods/js/index.js
@@ -105,14 +105,13 @@ var total = 0;
 $(document).ready(function() {
   window.onscroll = function() { myFunction(); };
 
-  var header = document.getElementById("header");
   var sticky = header.offsetTop;
 
   function myFunction() {
     if (window.pageYOffset > sticky) {
-      header.classList.add("sticky");
+      $('#header').addClass("sticky");
     } else {
-      header.classList.remove("sticky");
+      $('#header').removeClass("sticky");
     }
   }
 
